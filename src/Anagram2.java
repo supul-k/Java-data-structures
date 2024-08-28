3a1ae1765955c455d80d0af3cfd7beb99e1f47bf
Supul Kalhara <supulkalhara7@gmail.com>
2024-08-28 07:51:31 +0530

    anagram2

---
+++ src/Anagram2.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Anagram2 {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) return new ArrayList<>();
+
+        Map< String, List<String>> map = new HashMap<>();
+
+        for ( String str : strs) {
+
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sorted = new String(charArray);
+
+            if (!map.containsKey(sorted)){
+                map.put( sorted, new ArrayList<>());
+            }
+
+            map.get(sorted).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+
+        Anagram2 ana = new Anagram2();
+
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+
+        List<List<String>> result = ana.groupAnagrams(strs);
+
+        // Print the result
+        for (List<String> group : result) {
+            System.out.println(group);
+        }
+    }
+}
+
